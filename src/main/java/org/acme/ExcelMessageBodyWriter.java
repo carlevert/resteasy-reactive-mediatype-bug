076bfc2514a4c9e754118f236e1cf48fa6ee4c87
Carl-Evert Kangas <carlevert.kangas@gmail.com>
2022-04-04 09:19:16 +0200

    Workaround

--- src/main/java/org/acme/ExcelMessageBodyWriter.java
+++ src/main/java/org/acme/ExcelMessageBodyWriter.java
@@ -1,6 +1,8 @@
 package org.acme;
 
 import javax.ws.rs.WebApplicationException;
+import javax.ws.rs.core.Context;
+import javax.ws.rs.core.HttpHeaders;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.MultivaluedMap;
 import javax.ws.rs.ext.MessageBodyWriter;
@@ -14,11 +16,18 @@ import java.nio.charset.StandardCharsets;
 @Provider
 public class ExcelMessageBodyWriter implements MessageBodyWriter<MyResponseEntity> {
 
+    @Context
+    HttpHeaders headers;
+
     @Override
     public boolean isWriteable(Class<?> aClass, Type type, Annotation[] annotations, MediaType mediaType) {
+//        return aClass.equals(MyResponseEntity.class) &&
+//                mediaType.getType().equals("application") &&
+//                mediaType.getSubtype().equals("vnd.ms-excel");
+
         return aClass.equals(MyResponseEntity.class) &&
-                mediaType.getType().equals("application") &&
-                mediaType.getSubtype().equals("vnd.ms-excel");
+                headers.getAcceptableMediaTypes().stream().anyMatch(t -> t.getType().equals("application") && t.getSubtype().equals("vnd.ms-excel"));
+
     }
 
     @Override
@@ -26,7 +35,7 @@ public class ExcelMessageBodyWriter implements MessageBodyWriter<MyResponseEntit
                         MediaType mediaType, MultivaluedMap<String, Object> multivaluedMap,
                         OutputStream outputStream) throws IOException, WebApplicationException {
 
-        outputStream.write(myResponseEntity.getData().getBytes(StandardCharsets.UTF_8));
+        outputStream.write((myResponseEntity.getData() + " vnd.ms-excel").getBytes(StandardCharsets.UTF_8));
     }
 
 }
--- src/main/java/org/acme/TextPlainMessageBodyWriter.java
+++ src/main/java/org/acme/TextPlainMessageBodyWriter.java
@@ -1,6 +1,8 @@
 package org.acme;
 
 import javax.ws.rs.WebApplicationException;
+import javax.ws.rs.core.Context;
+import javax.ws.rs.core.HttpHeaders;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.MultivaluedMap;
 import javax.ws.rs.ext.MessageBodyWriter;
@@ -9,18 +11,25 @@ import java.io.IOException;
 import java.io.OutputStream;
 import java.lang.annotation.Annotation;
 import java.lang.reflect.Type;
+import java.nio.charset.StandardCharsets;
 
 @Provider
 public class TextPlainMessageBodyWriter implements MessageBodyWriter<MyResponseEntity> {
 
+    @Context
+    HttpHeaders headers;
+
     @Override
     public boolean isWriteable(Class<?> aClass, Type type, Annotation[] annotations, MediaType mediaType) {
-        return aClass.equals(MyResponseEntity.class) && mediaType.isCompatible(MediaType.TEXT_PLAIN_TYPE);
+//        return aClass.equals(MyResponseEntity.class) && mediaType.isCompatible(MediaType.TEXT_PLAIN_TYPE);
+
+        return aClass.equals(MyResponseEntity.class) &&
+                headers.getAcceptableMediaTypes().stream().anyMatch(t -> t.getType().equals("text") && t.getSubtype().equals("plain"));
     }
 
     @Override
     public void writeTo(MyResponseEntity myResponseEntity, Class<?> aClass, Type type, Annotation[] annotations, MediaType mediaType, MultivaluedMap<String, Object> multivaluedMap, OutputStream outputStream) throws IOException, WebApplicationException {
-        outputStream.write("text/plain".getBytes());
+        outputStream.write(myResponseEntity.getData().getBytes(StandardCharsets.UTF_8));
     }
 
 }
